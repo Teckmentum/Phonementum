47be6bcd31af998d832798924fad0638ac8a0462
GLORIMAR CASTRO-NORIEGA <gcastro@e-taxes.us>
2019-02-08 18:05:40 -0400

    global variables

---
+++ global_settings.py
@@ -0,0 +1,14 @@
+"""File para inicializar las variables globales
+
+Variables de token se deben inicializar aqui
+
+"""
+
+import os
+
+"""
+TWILIO VARIABLES
+"""
+twilio_sid      = os.environ["twilio_sid"]
+twilio_token    = os.environ["twilio_token"]
+
--- phone/views.py
+++ phone/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from twilio.rest import Client
+
 
 # Create your views here.
+
+#variables para auth con Twilio
+account_sid     = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token      = 'your_auth_token'
+client          = Client(account_sid, auth_token)
 
 
 def make_call():
