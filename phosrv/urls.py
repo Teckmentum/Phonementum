d8278a1407c99e96827e4ce8b0b1fe38b9c274f3
GLORIMAR CASTRO-NORIEGA <gcastro@e-taxes.us>
2019-02-12 14:12:23 -0400

    voice incall_lobby

--- phosrv/urls.py
+++ phosrv/urls.py
@@ -19,6 +19,5 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('phone/', include('phone.urls')),  #incluye todos los url dentro del app phone
     path('voice/', include('voice.urls'))
 ]
---
+++ voice/urls.py
@@ -0,0 +1,24 @@
+"""URLS para voice/*
+
+Todos los urls relacionados con el voice app estan declarados aqui.
+
+
+Notes
+-----
+    Autor: Glorimar Castro-Noriega
+    Creado: Febrero-12-2019
+
+"""
+
+
+from django.contrib import admin
+from django.urls import path
+
+from voice import views
+
+urlpatterns = [
+    path('lobby', views.incall_lobby()) #path completo voice/lobby
+
+
+]
+
