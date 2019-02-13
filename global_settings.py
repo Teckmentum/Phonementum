a6429623796ee92129b15ed54a66f8ea4e6178f3
GLORIMAR CASTRO-NORIEGA <gcastro@e-taxes.us>
2019-02-12 22:37:27 -0400

    fix global variables

--- global_settings.py
+++ global_settings.py
@@ -12,6 +12,6 @@ TWILIO VARIABLES
 twilio_sid      = os.environ["twilio_sid"]
 twilio_token    = os.environ["twilio_token"]
 twilio_etaxes_workspace_sid = os.environ["twilio_workspace_etaxes_sid"]
-twilio_etaxes_workflow_sid  = {"soporte": os.environ["twilio_etaxes_workflow_soporte"], "ventas": os.environ["twilio_etaxes_workflow_venta"]}
+twilio_etaxes_workflow_sid  = {"soporte": os.environ["twilio_etaxes_workflow_soporte"], "ventas": os.environ["twilio_etaxes_workflow_ventas"]}
 twilio_num_etax_fl  = "+18634003829"
 
