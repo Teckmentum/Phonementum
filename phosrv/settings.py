2a5bb916da7a619a8ed3f5bf3062818319d5aaca
Victor <sintravago@gmail.cm>
2019-02-14 10:10:49 -0400

    Conexion a la base de datos

--- phosrv/settings.py
+++ phosrv/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'phosrv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': '190.202.1.53',
+        'USER': 'django',
+        'PASSWORD': 'zxasqw123',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
     }
 }
 
