28838caa24c316ed6d544d8537a2a15f91d23554
glorimarcastro <teckmentum@gmail.com>
2019-02-17 14:07:10 -0400

    Update views.py

--- voice/views.py
+++ voice/views.py
@@ -132,7 +132,7 @@ def retrun_mp3():
 
 @csrf_exempt
 def sip_redirect(request):
-    request = (HttpRequest)(request)
+    #request = (HttpRequest)(request)
     if request.method == "POST":
         print(request.POST)
     elif request.method == "GET":
