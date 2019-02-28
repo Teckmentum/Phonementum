f8d0041122cbac77e4c4d383d1f9b35d8f9f55ab
glorimarcastro <teckmentum@gmail.com>
2019-02-28 00:12:54 -0400

    Update settings.py

--- phosrv/settings.py
+++ phosrv/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'phosrv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ["postgres_database"],
+        'USER': os.environ["postgres_user"],
+        'PASSWORD': os.environ["postgres_password"],
+        'HOST': os.environ["postgres_host"],
+        'PORT': '5432'
     }
 }
 
