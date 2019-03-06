b5bda3bbfd37d71099317e76f681755e6c66b992
GLORIMAR CASTRO-NORIEGA <gcastro@e-taxes.us>
2019-03-06 00:01:59 -0400

    voice_call

--- db_manager/db_getters.py
+++ db_manager/db_getters.py
@@ -9,21 +9,7 @@ from psycopg2.extensions import new_type, register_type
 import xml.dom.minidom as xmldom
 
 
-"""Modulo que devuelve el xml del lobby para el numero indicado en to_number
-
-
-"""
-
-
 # this methods are to cast xml data from postgres to python xml object
-def cast_xml(value, cur):
-    if value is None:
-        return None
-    #return ET.fromstring(value)
-    return value
-
-
-register_type(new_type((142,), "XML", cast_xml))
 
 
 def get_lobby_xml(to_number):
@@ -36,7 +22,7 @@ def get_department_xml(to_number, department):
     return to_number + "_dept_" + department + ".xml"
 
 
-def get_twiml_xml(id=None, table=None, phone=None):
+def get_twiml_xml(id=None, table=None, phone=None, gather=None):
     """
 
     Args:
@@ -46,37 +32,48 @@ def get_twiml_xml(id=None, table=None, phone=None):
 
     Returns:
         dict: {'isValid': bool, 'error': str, 'status': int, 'twiml_xml': str}
+
+    Notes:
+        1. Author: Glorimar Castro-Noriega
+        2. Date: 3-5-19
     """
 
-    # validate id and table param
-    result = validations.validate_id_table(id=id, table_name=table)
+    # validate id and table param, also gather is it is passed
+    if gather is None:
+        result = validations.validate_id_table(id=id, table_name=table)
+    else:
+        result = validations.validate_gather_id_table(id=id, table_name=table, gather=gather)
+
     if result['error'] is None and result['isValid']:
-        #validate phone param
-        if phone is not None:
-            query = "SELECT twiml_xml FROM phonementum.%s where %s = '%s' and phone = '%s'" % (table,
-                                                                                               validations.ID_NAMES[table],
-                                                                                               id, phone.replace("+", ""))
+        # validate phone param
+        if gather is None and phone is None:
+            result['error'] = True
+            result['messege'] = db_errors.ArgsCantBeNone('get_twiml_xml', 'id', 'table', 'phone or gather').message
+        else:
+            query_option = "gather = '%s'" % gather if gather is not None else "phone = '%s'" % phone.replace("+", "")
+            query = "SELECT twiml_xml FROM phonementum.%s " \
+                    "where %s = '%s' and %s" % (table, validations.ID_NAMES[table],
+                                                id, query_option)
             conn, cur = connect.connect2django()
 
             cur.execute(query)
             result['twiml_xml'] = cur.fetchone()
-            result['twiml_xml'] = result['twiml_xml'][0]
-            connect.close_db_connection(cur=cur, conn=conn) # todo manejar errores en la coneccion del db
-        else:
-            result['error'] = True
-            result['messege'] = db_errors.ArgsCantBeNone('get_twiml_xml', 'id', 'table', 'phone').message
-        # look at db
+            connect.close_db_connection(cur=cur, conn=conn)  # todo manejar errores en la coneccion del db
+
+            # look at db
 
     # if twiml_xml is None element was not found
     if result['twiml_xml'] is None:
         result['error'] = True
         result['messege'] = "An element for the given id at the given table was not found"
         result['status'] = 400
+    else:
+        result['twiml_xml'] = result['twiml_xml'][0]  # fetchone return a tuple here we are selecting the real value
 
-    #print(result)
+    print(result)
     #print(ET.tostring(result['twiml_xml'][0], encoding='utf8', method='xml'))
 
     return result
 
 
-#get_twiml_xml('0031765-0048', 'site_dba_phone', '17877860324')
+#get_twiml_xml(id='660902047', table='company_options', gather='1')
--- voice/views.py
+++ voice/views.py
@@ -7,7 +7,7 @@ import global_settings as gv
 from voice.twilio2voice_handeler import voice_helpers
 from xml.etree import cElementTree as ET
 
-client = Client(gv.twilio_sid, gv.twilio_token)
+
 # Create your views here.
 
 def index(reques):
@@ -54,123 +54,6 @@ def incall_department(request):
     # devolver el xml
     return HttpResponse(open(to_phone + "_dep_" + department_id + ".xml").read())
 
-@csrf_exempt
-def assignment_callback(request):
-    #request = (HttpRequest)(request)
-    print("---------------------------------------------------------------")
-    if request.method == "GET":
-        print(request.GET)
-    elif request.method == "POST":
-        print(request.POST)
-    print("---------------------------------------------------------------")
-    return HttpResponse({}, content_type="application/json")
-
-@csrf_exempt
-def create_task(request):
-    print(gv.twilio_etaxes_workspace_sid + "         " + gv.twilio_etaxes_workflow_sid["soporte"])
-    task = client.taskrouter.workspaces(gv.twilio_etaxes_workspace_sid).tasks.create(workflow_sid=gv.twilio_etaxes_workflow_sid["soporte"], attributes='{"selected_soporte":"3"}')
-    print(task.attributes)
-    return HttpResponse({},content_type="application/json")
-
-@csrf_exempt
-def accept_reservation(request):
-    #request = (HttpRequest)(request)
-    #get task sid and reservation sid
-    if request.method == 'GET':
-        task_sid = request.GET.get("task_sid")
-        reservation_sid = request.GET.get("reservation_sid")
-    elif request.method == "POST":
-        task_sid = request.POST.get("task_sid")
-        reservation_sid = request.POST.get("reservation_sid")
-
-    reservation = client.taskrouter.workspaces(gv.twilio_etaxes_workspace_sid)\
-                    .tasks(task_sid).reservations(reservation_sid)\
-                    .update(reservation_status='accepted')
-    print(reservation.reservation_status)
-    return HttpResponse({}, content_type="application/json")
-
-
-"""Enqueue a selected task
-Description:
--------------
-    Este metodo se encarga de crear un task para TaskRouter y 
-    lo manda a queue con el workflow indicado
-
-Notes:
--------------
-    Author:  Glorimar
-    Created: Feb-13-2019
-"""
-@csrf_exempt
-def enqueue_call(request, workspace, workflow, task_attributes):
-    #request = (HttpRequest)(request)
-    print("Working in workspace:" + workspace + " workflow: " + workflow)
-
-    #get user selection
-    digit = None
-    if request.method == 'POST':
-        digit = request.POST.get("Digits")
-    elif request.method == 'GET':
-        digit = request.GET.get("Digits")
-
-    #set task attributes
-    task_json = '{"' + task_attributes + '":"' + str(digit) + '"}'
-
-    #enqueue task
-    resp = VoiceResponse()
-    enqueue = resp.enqueue(None, workflow_sid=gv.twilio_etaxes_workflow_sid[workflow], wait_url="http://phonementum.herokuapp.com/voice/hold")
-    enqueue.task(task_json)
-    resp.append(enqueue)
-    return HttpResponse(str(resp))
-
-@csrf_exempt
-def retrun_mp3():
-    #file = open("/path/to/my/song.mp3", "rb").read()
-    #response['Content-Disposition'] = 'attachment; filename=filename.mp3'
-    #return HttpResponse(file, mimetype="audio/mpeg")
-    pass
-
-"""Redirect call to a sip endpoint
-Description:
-------------
-    Este metodo recibe como query parametrs: domain and department.
-    Depende del dominio y el departamento return un xml con instrucciones a que sip redirigir la llamada
-
-Notes:
--------
-    Author: Glorimar Castro
-"""
-@csrf_exempt
-def sip_redirect(request):
-    #request = (HttpRequest)(request)
-
-    #get parametrs
-    digits = None
-    to = None
-    domain = None
-    department = None
-    if request.method == "POST":
-        digit       = request.POST.get("Digits")
-        to          = request.POST.get("To")
-        domain      = request.GET.get("domain")
-        department  = request.GET.get("department")
-    elif request.method == "GET":
-        pass
-    #return el xml adecuado dependiendo de los parametros
-    print("devolviendo el file: " + to + "_dept_" + department + "_sel_" + digit + ".xml")
-    return HttpResponse(open(to + "_dept_" + department + "_sel_" + digit + ".xml").read())
-
-@csrf_exempt
-def hold_xml(request):
-    # ------------------------
-    # extraer a quien se llama
-    # ------------------------
-
-
-    # buscar en la base de datos el xml de este numero
-
-    # devolver el xml
-    return HttpResponse(open("+18634003829_soporte_validacion.xml").read())
 
 @csrf_exempt
 def callUmbrellaCo(request):
@@ -266,9 +149,9 @@ def voice_call(request):
     # get and validate parameters, also handle any error
     parameters = voice_helpers.extract_parameters(request)
     if parameters['error'] is False:
-        # todo get and return xml
         # get xml from db if id exist in table if not return error
-        twiml_xml = db_getters.get_twiml_xml(parameters['id'], table=parameters['table'], phone=parameters['phone'])
+        twiml_xml = db_getters.get_twiml_xml(parameters['id'], table=parameters['table'],
+                                             phone=parameters['phone'])
         print(twiml_xml)
         if twiml_xml['error'] is True:
             response = HttpResponse(twiml_xml['messege'], status=twiml_xml['status'])
