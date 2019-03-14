0a2049d6895378349df168d5c4151009dce20b44
GLORIMAR CASTRO-NORIEGA <gcastro@e-taxes.us>
2019-03-13 20:24:08 -0400

    Update views.py

--- hermes/views.py
+++ hermes/views.py
@@ -83,10 +83,8 @@ def incoming_voice_call_from_lobby(request):
         return HttpResponse(parameters[gv.MESSAGE], status=parameters['status'])
 
     # get twiml from hermessession
-    print(request.session.keys())
-    print(request.session[gv.CALL_SID])
-    if parameters[gv.CALL_SID] in request.session.keys() and gv.TWILIOML_AFTER_LOBBY in request.session[gv.CALL_SID].keys():
-        return HttpResponse(request.session.get(gv.CALL_SID)[gv.TWILIOML_AFTER_LOBBY])
+    if parameters[gv.CALL_SID] in request.session.keys() and gv.TWILIOML_AFTER_LOBBY in request.session[parameters[gv.CALL_SID]].keys():
+        return HttpResponse(request.session[parameters[gv.CALL_SID]][gv.TWILIOML_AFTER_LOBBY])
     else:
         return HttpResponse("For %s and twiml after lobby wasnt found" % (parameters[gv.CALL_SID]), status=400)
 
